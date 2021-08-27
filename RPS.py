3a8faa05a2cac3dca9e422fe5ac804e4e778aed1
OU IT <43416443+TaylorPB@users.noreply.github.com>
2021-08-27 16:18:45 -0400

    Update RPS.py
    Logic issues.

--- RPS.py
+++ RPS.py
@@ -1,28 +1,19 @@
-import math   ## Completely useless need for math class
 from random import randrange
 
 
-User_Choice = ""  
+User_Choice = ""
 
-
-## Global Variable
 Rock = 'rock'
 Paper = 'paper'
 Sissors = 'sissors'
 
-
-
-## I can't spell.
 def RNP():
 
     print( "ROCK PAPER SISSORS SHOOT!....... \n")
 
-    
-    
-    
 def Basic_Function():
     User_Choice = ""
-    ## Makes sure the user's answers are in line or makes sure the user actually answers 
+    Final_Choice = User_Choice
 
     while(len(User_Choice)) == 0 or (Paper not in User_Choice) or (Rock not in User_Choice) or (Sissors not in User_Choice):
         User_Choice = input("Please pick betweeen Rock , Paper, Sissors")
@@ -39,29 +30,23 @@ def Basic_Function():
 
             return User_Choice
 
-          
-          
- # If statements for each choice         
 def Logic_of_Game():
 
    Answer = Basic_Function()
    RPS_List = ['rock', 'paper', 'sissors']
-   Decider = randrange(0, 3)    # Will pick through the different index of the  RPS_list 
+   Decider = randrange(0, 3)
 
    print(Answer.capitalize(), "VS ",RPS_List[Decider].capitalize() )
-  
-  
-  #A BUNCH OF IF STATEMENTS 
 
    if( Rock in RPS_List[Decider]):
 
         if( Rock in Answer):
             print(" It is a tie!")
 
-        if (Sissors in Answer):
+        if (Paper in Answer):
             print("Sorry you lose :(")
 
-        if(Paper in Answer ):
+        if(Sissors in Answer ):
             print( " Winner winner chicken dinner!")
 
    if (Paper in RPS_List[Decider]):
@@ -69,10 +54,10 @@ def Logic_of_Game():
        if (Paper in Answer):
            print(" It is a tie!")
 
-       if (Sissors in Answer):
+       if (Rock in Answer):
            print("Sorry you lose :(")
 
-       if (Rock in Answer):
+       if (Sissors in Answer):
            print(" Winner winner chicken dinner!")
 
    if (Sissors in RPS_List[Decider]):
